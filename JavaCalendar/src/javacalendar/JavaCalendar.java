b6d505e92a0bd6aef961945fe5e2d49ebfbca192
Shahied Rustin <97807830+snowyru@users.noreply.github.com>
2023-10-21 23:54:35 +0200

    fin timout

---
+++ JavaCalendar/src/javacalendar/JavaCalendar.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javacalendar;
+
+/**
+ *
+ * @author sniffel
+ */
+public class JavaCalendar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainFrame mainframe = new MainFrame();
+        mainframe.setVisible(true);
+    }
+    
+}
